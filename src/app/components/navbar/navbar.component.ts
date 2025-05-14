4744c0004acccb78bffc71e36fe09a28950083fd
Erick <eric.paredes@duocuc.cl>
2025-05-14 18:17:02 -0400

    boton de cerrar sesión

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -12,10 +12,18 @@ import { Output, EventEmitter } from '@angular/core';
 })
 export class NavbarComponent {
   menuOpen = true;
+  constructor(private router: Router) {} // Inyecta el Router
 
   @Output() menuState = new EventEmitter<boolean>();
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
     this.menuState.emit(this.menuOpen); // emite el estado
+  }
+
+  cerrarSesion() {
+    // Aquí puedes agregar la lógica para cerrar la sesión
+    // Por ejemplo, limpiar tokens, eliminar datos del localStorage, etc.
+    console.log('Cerrando sesión...');
+    this.router.navigate(['/login']); // Redirige a la página de inicio de sesión
   }
 }
